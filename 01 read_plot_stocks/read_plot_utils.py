f19298bf30bad5d544d3be93356516e7e9b50c1a
Trang Nguyen <ntrang086@gmail.com>
2017-11-27 17:19:12 -0500

    feat: Create helper functions to read and plot multiple stocks
    function symbol_to_path returns CSV file path given ticker symbol.
    function get_data read stock data (adjusted close) for given symbols from CSV files.

---
+++ 01 read_plot_stocks/read_plot_utils.py
@@ -0,0 +1,45 @@
+"""Utility functions"""
+
+import os
+import pandas as pd
+
+def symbol_to_path(symbol, base_dir="../../data"):
+    """Return CSV file path given ticker symbol."""
+    return os.path.join(base_dir, "{}.csv".format(str(symbol)))
+
+
+def get_data(symbols, dates):
+    """Read stock data (adjusted close) for given symbols from CSV files."""
+    df = pd.DataFrame(index=dates)
+    if 'SPY' not in symbols:  # add SPY for reference, if absent
+        symbols.insert(0, 'SPY')
+
+    # Read and join data for each symbol
+    for symbol in symbols:
+        df_temp = pd.read_csv(symbol_to_path(symbol), index_col='Date', 
+        parse_dates=True, usecols=['Date', 'Adj Close'], na_values=['nan'])
+        
+        # Rename to 'Adj Close' for each symbol to prevent clash
+        df_temp = df_temp.rename(columns={'Adj Close': symbol})
+        
+        df = df.join(df_temp)
+        if symbol == 'SPY': # drop dates SPY didn't trade
+            df = df.dropna(subset=['SPY'])
+
+    return df
+
+
+def test_run():
+    # Define a date range
+    dates = pd.date_range('2010-01-22', '2010-01-26')
+
+    # Choose stock symbols to read
+    symbols = ['GOOG', 'IBM', 'GLD']
+    
+    # Get stock data
+    df = get_data(symbols, dates)
+    print (df)
+
+
+if __name__ == "__main__":
+    test_run()
