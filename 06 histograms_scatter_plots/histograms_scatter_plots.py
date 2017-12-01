3058ae1cd85384f8b0451970e1b769493cd9408b
Trang Nguyen <ntrang086@gmail.com>
2017-12-01 18:16:34 -0500

    feat: Creat scatter plots
    Two scatter plots: SPY vs. XOM and SPY vs. GLD and their corresponding best fitting lines.

--- 06 histograms_scatter_plots/histograms_scatter_plots.py
+++ 06 histograms_scatter_plots/histograms_scatter_plots.py
@@ -1,8 +1,9 @@
-"""Stat analysis for time series"""
+"""Histograms and scatter plots"""
 
 import os
 import pandas as pd
 import matplotlib.pyplot as plt
+import numpy as np
 
 def symbol_to_path(symbol, base_dir="../../data"):
     """Return CSV file path given ticker symbol."""
@@ -42,7 +43,7 @@ def test_run():
     dates = pd.date_range("2009-01-01", "2012-12-31")
 
     # Choose stock symbols to read
-    symbols = ["SPY", "XOM"]
+    symbols = ["SPY", "XOM", "GLD"]
     
     # Get stock data
     df = get_data(symbols, dates)
@@ -55,6 +56,45 @@ def test_run():
     daily_returns["XOM"].hist(bins=20, label="XOM")
     plt.legend(loc="upper right")
     plt.show()
+    # Another way to plot the above histogram
+    #daily_returns.plot(kind="hist", bins=20, y=["SPY", "XOM"])
+
+
+    # Scatterplot SPY vs. XOM and SPY vs. GLD and the respective best fitting lines
+    # Two subplots appear side by side
+    f, (ax1, ax2) = plt.subplots(1, 2, sharey=True)
+    
+    # The first subplot SPY vs. XOM and its best fitting line
+    ax1.scatter(daily_returns["SPY"], daily_returns["XOM"])
+    beta_XOM, alpha_XOM = np.polyfit(daily_returns["SPY"], daily_returns["XOM"], 1)
+    print ("beta_XOM = {}\nalpha_XOM = {}".format(beta_XOM, alpha_XOM))
+    ax1.plot(daily_returns["SPY"], beta_XOM*daily_returns["SPY"] + alpha_XOM, "-", color="r")
+    
+    # The first subplot SPY vs. GLD and its best fitting line
+    ax2.scatter(daily_returns["SPY"], daily_returns["GLD"])
+    beta_GLD, alpha_GLD = np.polyfit(daily_returns["SPY"], daily_returns["GLD"], 1)
+    print ("beta_GLD = {}\nalpha_GLD = {}".format(beta_GLD, alpha_GLD))
+    ax2.plot(daily_returns["SPY"], beta_GLD*daily_returns["SPY"] + alpha_GLD, "-", color="r")
+    
+    plt.show()
+
+
+    """
+    # Plot the above scatterplots into separate graphs
+    # Scatterplot SPY vs. XOM    
+    daily_returns.plot(kind="scatter", x="SPY", y="XOM")
+    beta_XOM, alpha_XOM = np.polyfit(daily_returns["SPY"], daily_returns["XOM"], 1)
+    print ("beta_XOM = {}\nalpha_XOM={}".format(beta_XOM, alpha_XOM))
+    plt.plot(daily_returns["SPY"], beta_XOM*daily_returns["SPY"] + alpha_XOM, "-", color="r")
+    plt.show()
+
+    # Scatterplot SPY vs. GLD    
+    daily_returns.plot(kind="scatter", x="SPY", y="GLD")
+    beta_GLD, alpha_GLD = np.polyfit(daily_returns["SPY"], daily_returns["GLD"], 1)
+    print ("beta_GLD = {}\nalpha_GLD={}".format(beta_GLD, alpha_GLD))
+    plt.plot(daily_returns["SPY"], beta_GLD*daily_returns["SPY"] + alpha_GLD, "-", color="r")
+    plt.show()
+    """
 
 
 if __name__ == "__main__":
